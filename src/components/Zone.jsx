85c293ae8b1f1012a709e626944966ac8412cd23
Pool Tasayco <pool.tasayco@gmail.com>
2024-03-02 03:04:04 -0500

    adding PlantCare project

---
+++ src/components/Zone.jsx
@@ -0,0 +1,3 @@
+export default function Zone() {
+    return <div>Zone</div>;
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,10 @@
+import Zone from "../components/Zone";
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-green-500">Esto es home, debajo está Zone</h1>
+            <Zone />
+        </>
+    );
+}
